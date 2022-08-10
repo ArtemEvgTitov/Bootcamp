396efa6508f8c23394e2f183954e6cd898d7ead0
ArtemEvgTitov <>
2022-08-10 22:39:18 +0300

    Задача решена

--- Bootcamp4_homework/Program.cs
+++ Bootcamp4_homework/Program.cs
@@ -20,18 +20,22 @@ int[] Shift(int[] array)
     }
     if (K < 0)
     {
-        for (int i = size - 1; i >= 0; i--)
+        int lastNumber = size - 1;
+        for (int i = lastNumber, j = 1; i >= 0; i--)
         {
-            if (i + K > 0)
+            if (i + K >= 0)
                 array2[i + K] = array[i];
-            if (i + K <= 0)
-                array2[i + K] = array[i];
+            if (i + K < 0)
+            {
+                array2[size - j] = array[i];
+                j++;
+            }
         }
     }
     return array2;
 }
 
-int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 int[] newArray = Shift(array);
 Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
 Console.WriteLine("Конечный массив: [" + string.Join(", ", newArray) + "]");
