493f4b10096096bca9654627800b2fd55ee43682
ArtemEvgTitov <>
2022-08-10 20:45:10 +0300

    создан метод для положительного числа K

--- Bootcamp4_homework/Program.cs
+++ Bootcamp4_homework/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Дана последовательность из N целых чисел и число K. 
+Необходимо сдвинуть всю последовательность (сдвиг - циклический) на |K| элементов вправо, 
+если K – положительное и влево, если отрицательное.*/
+
+int[] Shift(int[] array)
+{
+    Console.Write("Введите число К: ");
+    int K = Convert.ToInt32(Console.ReadLine());
+    int size = array.Length;
+    int[] array2 = new int[size];
+    if (K > 0)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (i + K < size)
+                array2[i + K] = array[i];
+            if (i + K >= size)
+                array2[i + K - size] = array[i];
+        }
+    }
+    if (K < 0)
+    {
+        for (int i = size - 1; i >= 0; i--)
+        {
+            if (i + K > 0)
+                array2[i + K] = array[i];
+            if (i + K <= 0)
+                array2[i + K] = array[i];
+        }
+    }
+    return array2;
+}
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int[] newArray = Shift(array);
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine("Конечный массив: [" + string.Join(", ", newArray) + "]");
