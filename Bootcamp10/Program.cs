6c86e8d301f410e71160c4c35f9464c65cf6569e
ArtemEvgTitov <>
2022-08-23 13:48:04 +0300

    Добавлен конспект 10 буткемпа

--- Bootcamp10/Program.cs
+++ Bootcamp10/Program.cs
@@ -2,9 +2,11 @@
 // Работает только и цифрами
 
 int[] array = { 0, 2, 3, 2, 1, 5, 9, 1, 1 };
-
+int[] array2 = { 0, 2, 3, 2, 20, 5, 9, 10, 1 };
+int[] sortedArray = CountingSortExtended(array2);
 CountingSort(array);
-Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(string.Join(", ", sortedArray));
+
 
 void CountingSort(int[] inputArray)
 {
@@ -26,3 +28,25 @@ void CountingSort(int[] inputArray)
     }
 }
 
+int[] CountingSortExtended(int[] inputArray)
+{
+    int max = inputArray.Max();
+    
+    int[] sortedArray = new int[inputArray.Length];
+    int[] counters = new int[max + 1];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i]]++;
+    }
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            sortedArray[index] = i;
+            index++;
+        }
+    }
+    return sortedArray;
+}
